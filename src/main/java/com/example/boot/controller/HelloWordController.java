eda2eeaf8c86bba88e4aaba9b8cc12afa42ec6d7
freddy <rasamimananafreddy@gmail.com>
2025-12-29 02:40:56 +0300

    Initial Spring Boot backend project

---
+++ src/main/java/com/example/boot/controller/HelloWordController.java
@@ -0,0 +1,18 @@
+package com.example.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Application mandeha tsara! ouiiiiii 🎉";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Ity ny test page!";
+    }
+}
