2e18a1f7905ea04bbe55e662eda977b58cc0b70d
freddy <rasamimananafreddy@gmail.com>
2026-01-07 22:46:50 +0300

    io e

--- src/main/java/com/example/boot/controller/UserController.java
+++ src/main/java/com/example/boot/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,4 +48,20 @@ public class UserController {
         userRepository.deleteById(id);
         return "User deleted with id: " + id;
     }
+    //mise a jour
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User newUserData) {
+        return userRepository.findById(id)
+            .map(user -> {
+                user.setNom(newUserData.getNom());
+                user.setEmail(newUserData.getEmail());
+                return userRepository.save(user);
+            })
+            .orElseGet(() -> {
+                // Raha tsy misy ilay id, afaka mamorona vaovao koa
+                newUserData.setId(id);
+                return userRepository.save(newUserData);
+            });
+}
+
 }
